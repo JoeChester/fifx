4f12cd93c61d0a15ab220382fcb636ddb3017c87
Jonas Kleinkauf <jonas.kleinkauf@bbraun.com>
2017-11-30 15:50:33 +0100

    first logic on saving games

--- main.js
+++ main.js
@@ -58,6 +58,15 @@ var pairings = [
     [1, 6]
 ]
 
+function validNumber(n) {
+    if(n === 0){
+        return true;
+    }
+    if (isNaN(n) || n === "") {
+        return false;
+    } return true;
+}
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (0 !== currentIndex) {
@@ -79,7 +88,7 @@ function setup(cb) {
 }
 
 function reset() {
-    setup(function(){
+    setup(function () {
         db.transaction(function (tx) {
             tx.executeSql('DELETE FROM FIFX');
             console.log("DELETED ALL!");
@@ -110,9 +119,9 @@ function new_turn() {
     player_names = shuffle(player_names);
     console.table(player_names);
     db.transaction(function (tx) {
-        for (let i = 0; i<player_names.length; i++) {
-            tx.executeSql('UPDATE FIFX SET p1_name=? where p1_id=?', [player_names[i], i+1]);
-            tx.executeSql('UPDATE FIFX SET p2_name=? where p2_id=?', [player_names[i], i+1]);
+        for (let i = 0; i < player_names.length; i++) {
+            tx.executeSql('UPDATE FIFX SET p1_name=? where p1_id=?', [player_names[i], i + 1]);
+            tx.executeSql('UPDATE FIFX SET p2_name=? where p2_id=?', [player_names[i], i + 1]);
         }
         console.log("PLAYERS INSERTED!");
     });
@@ -121,4 +130,55 @@ function new_turn() {
 
 function load_turn() {
 
+}
+
+function save_game(id) {
+    let p1_name = document.getElementById("game" + id + "_p1").innerText;
+    let p2_name = document.getElementById("game" + id + "_p2").innerText;
+    let p1_score = document.getElementById("game" + id + "_p1_score").value;
+    let p2_score = document.getElementById("game" + id + "_p2_score").value;
+    if (!validNumber(p1_score) || !validNumber(p2_score)) {
+        alert("Ivalid Scores!");
+        return;
+    }
+    let p1_points = 0;
+    let p2_points = 0;
+    if (p1_score == p2_score) {
+        p1_points = 1;
+        p2_points = 1;
+    } else if (p1_score > p2_score) {
+        p1_points = 3;
+    } else if (p2_score > p1_score) {
+        p2_points = 3;
+    }
+    db.transaction(function (tx) {
+        tx.executeSql('UPDATE FIFX SET p1_score=?, p2_score=?, p1_points=?, p2_points=? where id=?', [p1_score, p2_score, p1_points, p2_points, id], function () {
+            //cb
+            refresh_game(id);
+        });
+    });
+}
+
+function refresh_game(id) {
+    db.transaction(function (tx) {
+        tx.executeSql('SELECT * FROM FIFX where id=?', [id], function (t, r) {
+            let id = r.rows[0].id;
+            let p1 = r.rows[0].p1_points;
+            let p2 = r.rows[0].p2_points;
+            console.log(id, p1, p2);
+            if (!validNumber(p1) || !validNumber(p2)) {
+                return;
+            }
+            if (p1 > p2) {
+                document.getElementById("game" + id + "_p1").className = "winner";
+                document.getElementById("game" + id + "_p2").className = "loser";
+            } else if (p2 > p1) {
+                document.getElementById("game" + id + "_p1").className = "loser";
+                document.getElementById("game" + id + "_p2").className = "winner";
+            } else if( p1 == p2 ){
+                document.getElementById("game" + id + "_p1").className = "draw";
+                document.getElementById("game" + id + "_p2").className = "draw";
+            }
+        });
+    });
 }
